50a306a8d71c6bf248dff1547823f79df32cde83
kifard <81812650+kifard@users.noreply.github.com>
2022-08-25 20:53:00 +0800

    Patch 8
    Co-Authored-By: AirKifMEm <110585293+AirKifMEm@users.noreply.github.com>

---
+++ My project/Assets/Events/ClaimRewardsButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaimRewardsButton : MonoBehaviour
+{
+
+    public int levalr;
+
+    public void Onclick()
+    {
+
+        if(levalr == EventData.level)
+        {
+
+        }
+
+    }
+}
